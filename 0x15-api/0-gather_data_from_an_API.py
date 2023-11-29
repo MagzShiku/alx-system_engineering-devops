cc6a515de1d91936e98e8b2f261f63e7177d6c27
Ubuntu <ubuntu@ip-172-31-19-71.eu-north-1.compute.internal>
2023-11-29 10:38:10 +0000

    0-gather_data_from_an_API.py_rev_03

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-   Python script that uses REST API (https://jsonplaceholder.typicode.com/)
+   Python script that uses REST API (https://jsonplaceholder.typicode.com/) 
    to return TODO list progress for a given employee ID
 
    Requirements:
@@ -11,13 +11,14 @@
 """
 
 import json
-import sys
+import sys 
 import urllib.request
 
 
 def get_employee_todo_list(employee_id):
+
     """
-       Retrieves TODO list by appending ID to URL
+       Retrieves TODO list by appending ID to URL  
        Reads and parses JSON response
     """
 
@@ -27,38 +28,34 @@ def get_employee_todo_list(employee_id):
     return todos
 
 
+def get_employee_name(employee_id):
+
+  """
+     Retrieves employee name from API
+  """
+
+  url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+  response = urllib.request.urlopen(url)
+  employee = json.loads(response.read())
+  return employee["name"]
+
+
 def display_employee_progress(employee_id):
-    """
-       Takes employee ID, calls get_employee_todo_list()
-       Displays progress in desired format using formatting
-       and loop to display completed tasks
-    """
 
-    todos = get_employee_todo_list(employee_id)
+  """
+     Displays progress in specified format
+  """
 
-    # Get employee name
-    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"  
-    employee_response = urllib.request.urlopen(employee_url)
-    employee = json.loads(employee_response.read())
-    employee_name = employee["name"]
+  todos = get_employee_todo_list(employee_id)
+  employee_name = get_employee_name(employee_id)
 
-    # Count completed tasks
-    completed_tasks = [todo for todo in todos if todo["completed"]]  
-    num_completed_tasks = len(completed_tasks)
-    total_num_tasks = len(todos)
-
-    # Display progress
-    print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_num_tasks}):")
-    for task in completed_tasks:
-        print(f"\t{task['title']}")
-
+  # rest of function
 
 if __name__ == "__main__":
-    """
-       Retrieves employee ID from command line arguments  
-       Calls display_employee_progress()
-    """
 
-    employee_id = int(sys.argv[1])
-    display_employee_progress(employee_id)
+  """
+     Calls main function
+  """
 
+  employee_id = int(sys.argv[1])
+  display_employee_progress(employee_id)
