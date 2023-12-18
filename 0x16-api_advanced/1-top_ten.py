343f6f7659e9d0362fd4c440a2a6ea8c09950f7d
Ubuntu <ubuntu@ip-172-31-19-71.eu-north-1.compute.internal>
2023-12-18 14:24:26 +0000

    top 10 reddits_rev-1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,16 +13,18 @@ def top_ten(subreddit):
     """
     this actions the requirements
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers)
+    response = requests.get(
+            f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10",
+            headers={"User-Agent": "My-User-Agent"},
+            allow_redirects=False
+    )
 
-    if response.status_code == 200:
+    if response.status_code >= 300:
+        print('None')
+    else:
         data = response.json()
         posts = data["data"]["children"]
 
         for post in posts:
             title = post["data"]["title"]
             print(title)
-    else:
-        print(None)
